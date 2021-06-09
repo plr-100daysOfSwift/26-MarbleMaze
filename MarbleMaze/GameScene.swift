f6f8e15a66fd8d368d714c2f65292d6c4623ac6a
Paul Richardson <developer@photographerafoot.com>
2021-06-10 08:46:10 +1000

    CHG: loadLevel()
    basic structure

--- MarbleMaze/GameScene.swift
+++ MarbleMaze/GameScene.swift
@@ -9,9 +9,33 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
-    override func didMove(to view: SKView) {
 
+	override func didMove(to view: SKView) {
+		loadLevel()
+	}
+
+	loadLevel() {
+		guard let levelURL = Bundle.main.url(forResource: "level1", withExtension: "txt") else { return }
+		guard let levelString = try? String(contentsOf: levelURL) else { return }
+
+		let lines = levelString.components(separatedBy: "\n")
+
+		for (row, line) in lines.reversed().enumerated() {
+			for (column, letter) in line.enumerated() {
+				if letter == "x" {
+					// load wall
+				} else if letter = "v" {
+					// load vortex
+				} else if letter = "s" {
+					// load star
+				} else if letter = "f" {
+					// load finish
+				} else if letter = " " {
+					// this is an empty space - do nothing
+				}
+			}
 		}
+
+	}
 
 }
